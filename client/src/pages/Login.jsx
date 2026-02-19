fce7d623fb4a28b040577de27258a31dd6278848
Big Eyes <genesisjames444@gmail.com>
2026-02-19 12:21:55 +0100

    Correct import path casing for Auth.css in Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import "../styles/auth.css";
+import "../styles/Auth.css";
 import { useAuth } from "../Context/AuthContext.jsx";
 
 function Login() {
